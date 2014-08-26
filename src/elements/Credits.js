29ffbdf308b6743cb90684c75aab419b24cfc3f7
roman_lubushkin <ra.lubushkin@gamil.com>
2014-08-26 18:01:48 +0900

    Merge hotfixes from RC-7.1.0

--- src/elements/Credits.js
+++ src/elements/Credits.js
@@ -45,6 +45,9 @@ anychart.elements.Credits = function() {
    * @private
    */
   this.domElement_ = null;
+
+  //disable by default at anychart related domains
+  this.enabled(!anychart.elements.Credits.DOMAIN_REGEXP.test(goog.dom.getWindow().location.hostname));
 };
 goog.inherits(anychart.elements.Credits, anychart.VisualBase);
 
@@ -104,6 +107,7 @@ anychart.elements.Credits.prototype.SUPPORTED_CONSISTENCY_STATES =
  * @example
  * anychart.licenseKey('AnyChart-95048498-10629dd6');
  * var chart = anychart.lineChart([10, 1, 7, 10]);
+ * chart.credits(true);
  * chart.credits().text('Changed credits');
  * chart.container(stage).draw();
  * @param {string=} opt_value Text value.
@@ -136,6 +140,7 @@ anychart.elements.Credits.prototype.text = function(opt_value) {
  * @example
  * anychart.licenseKey('AnyChart-95048498-10629dd6');
  * var chart = anychart.lineChart([10, 1, 7, 10]);
+ * chart.credits(true);
  * chart.credits().url('http://www.anychart.com/buy/');
  * chart.container(stage).draw();
  * @param {string=} opt_value Url value.
@@ -168,6 +173,7 @@ anychart.elements.Credits.prototype.url = function(opt_value) {
  * @example
  * anychart.licenseKey('AnyChart-95048498-10629dd6');
  * var chart = anychart.lineChart([10, 1, 7, 10]);
+ * chart.credits(true);
  * chart.credits().alt('Custom alternative text.');
  * chart.container(stage).draw();
  * @param {string=} opt_value Title value.
@@ -200,6 +206,7 @@ anychart.elements.Credits.prototype.alt = function(opt_value) {
  * @example
  * anychart.licenseKey('AnyChart-95048498-10629dd6');
  * var chart = anychart.lineChart([10, 1, 7, 10]);
+ * chart.credits(true);
  * chart.credits().logoSrc(null);
  * chart.container(stage).draw();
  * @param {string=} opt_value Logo source value.
