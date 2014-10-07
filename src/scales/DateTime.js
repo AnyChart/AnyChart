6e4c367b649ff4f25af2d6b3c92a3fb561c9855d
Grasshopper <grasshoppergn@gmail.com>
2014-10-07 13:09:33 +0900

    DVF-992 DateTime scale ticks fixes again

--- src/scales/DateTime.js
+++ src/scales/DateTime.js
@@ -136,13 +136,13 @@ anychart.scales.DateTime.prototype.calculate = function() {
 
   goog.base(this, 'calculate');
 
-  var newRange = this.minorTicks().setupAsMinor(this.min, this.max, this.minimumModeAuto, this.maximumModeAuto);
+  var newRange = this.ticks().setup(this.min, this.max, this.minimumModeAuto, this.maximumModeAuto);
+  this.minorTicks().setupAsMinor(this.min, this.max, newRange[0], newRange[1]);
+  // adjusting range AFTER minors calc to avoid range selection change
   if (this.minimumModeAuto)
     this.min = newRange[0];
   if (this.maximumModeAuto)
     this.max = newRange[1];
-
-  this.ticks().setup(this.min, this.max);
 
   this.range = this.max - this.min;
 };
