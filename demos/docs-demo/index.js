799a566e5dbb8fc26f79ff52d184f4c88fee9978
Vitaly Radionov <vitaly.radionov@anychart.com>
2014-10-13 10:15:30 +0900

    merge DVF-975-anychart.Base

--- demos/docs-demo/index.js
+++ demos/docs-demo/index.js
@@ -1,4 +1,6 @@
-var chart;
+var label1, label2;
+var radiusPixel = 0;
+
 function load() {
   var container = 'container';
   var stage = acgraph.create(container, 400, 300);
@@ -6,24 +8,26 @@ function load() {
   stage.rect(1, 1, stage.width() - 2, stage.height() - 2).fill('none').stroke('0.5 #000');
   /////////////////////////////////////////////////////////
 
+  //   chart = anychart.lineChart();
 
-  chart = anychart.columnChart();
-  chart.column([1.1, 1.6, 1.4, 1.9]).hoverFill('red');
+
+  var chart = anychart.lineChart();
+
+  var line = chart.line([1, 4, 2, 6]);
+  var title = chart.title();
+  title.text('MouseOver the title and click on line series.');
+  var counter = 0;
+  line.listen(anychart.enums.EventType.POINT_MOUSE_OUT, function(e){
+    title.fontColor('green');
+  });
+  line.listen(anychart.enums.EventType.POINT_MOUSE_OVER, function(e){
+    title.fontColor('red');
+  });
+  line.listen(anychart.enums.EventType.POINT_CLICK, function(e){
+    title.text('You can\'t click here anymore.').fontColor('black');
+    line.removeAllListeners();
+  });
+
   chart.container(stage).draw();
-  console.log('CAT:action(mouseClick,205,163)');
-  console.log('CAT:action(mouseMove,275,130)');
-  console.log('CAT:action(mouseMove,354,208)');
 
-}
-function resizeChart(w, h) {
-  w = w || chart.width() || 400;
-  h = h || chart.height() || 300;
-  w+=10;
-  h+=10;
-  console.log('resize chart from (', chart.width(), chart.height(), ') to (', w, h, ')');
-  chart.width(w);
-  chart.height(h);
-}
-function log(var_args){
-  console.log.apply(console, arguments);
 }
