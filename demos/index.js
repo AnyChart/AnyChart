b8526935a1acd9706d765e054ee14792e545020f
Sergey Medvedev <blackart.ru@gmail.com>
2014-11-13 22:43:09 +0800

    DVF-1084 image fill fail

--- demos/index.js
+++ demos/index.js
@@ -1,49 +1,31 @@
-anychart.onDocumentLoad(function() {
-  var stage = acgraph.create('container',600, 400);
-  var chart = anychart.lineChart();
-  acgraph.events.listen(stage, acgraph.events.EventType.RENDER_FINISH, function (e){
-    document.getElementById('status').innerHTML = 'chart draw';
-    console.log('chart draw');
-  });
-  stage.suspend();
+var chart
+anychart.onDocumentReady(function() {
 
+  //data
   var data = anychart.data.set([
-    ['P1', 178, 165, 154, 143, 132],
-    ['P2', 167, 153, 144, 133, 122]
+    {x: 'January', y: 10000},
+    {x: 'February', y: 12000},
+    {x: 'March', y: 18000},
+    {x: 'April', y: 11000},
+    {x: 'May', y: 9000}
   ]);
-  var data1 = data.mapAs({x: [0], value: [1]});
-  var data2 = data.mapAs({x: [0], value: [2]});
-  var data3 = data.mapAs({x: [0], value: [3]});
-  var data4 = data.mapAs({x: [0], value: [4]});
-  var data5 = data.mapAs({x: [0], value: [5]});
-  chart.marker(data1)
-      .hoverSize(30)
-      .size(20)
-      .hatchFill('diagonalcross')
-      .hoverHatchFill('diagonalcross')
-      .type('star5');
-  chart.marker(data2)
-      .hoverSize(50)
-      .size(20)
-      .hatchFill('diagonalbrick')
-      .type('star4');
-  chart.marker(data3)
-      .hoverSize(30)
-      .size(20)
-      .hoverHatchFill('horizontal')
-      .hatchFill('zigzag');
-  chart.marker(data4)
-      .hoverSize(30)
-      .size(20)
-      .hatchFill('checkerboard')
-      .type('square');
-  chart.marker(data5)
-      .hoverSize(30)
-      .size(20)
-      .hatchFill('percent30')
-      .hoverHatchFill('vertical')
-      .type('circle');
-  chart.container(stage);
-  chart.draw();
-  stage.resume();
+
+  //chart type
+  chart = anychart.areaChart();
+
+  //chart title
+  chart.title().text('Image fill');
+
+  //set axes titles
+  chart.xAxis().title().text('Retail Channel');
+  chart.yAxis().title().text('Sales');
+
+  //adjust chart fill mode and set picture instead of color
+  chart.area(data).fill({
+    src: 'http://bm.img.com.ua/berlin/storage/orig/d/0a/c6b96679df0f6b09766f96c9ce7150ad.jpg',
+    mode: acgraph.vector.ImageFillMode.TILE
+  });
+
+  //draw chart
+  chart.container('container').draw();
 });
