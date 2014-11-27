99772cc2160ea3f85b5b214110523f54c4724739
roman_lubushkin <ra.lubushkin@gamil.com>
2014-11-27 13:27:50 +0800

    Update activity oriented chart demo

--- demos/gantt/activity_oriented_chart.js
+++ demos/gantt/activity_oriented_chart.js
@@ -5,6 +5,19 @@ anychart.onDocumentReady(function(){
   chart.bounds(0, 0, '100%', '100%');
   chart.data(treeData);
 
+  var dataGrid = chart.getDataGrid();
+  dataGrid.column(0).title().text('#');
+  dataGrid.column(1).width(200);
+  dataGrid.column(2).width(70).textFormatter(function(item) {
+    var date = new Date(item.get('actualStart'));
+    return date.getFullYear() + '.' + date.getMonth() + '.' + date.getDay();
+  }).title().text('Start Time');
+
+  dataGrid.column(3).width(70).textFormatter(function(item) {
+    var date = new Date(item.get('actualStart'));
+    return date.getFullYear() + '.' + date.getMonth() + '.' + date.getDay();
+  }).title().text('End Time');
+
   chart.draw();
 
   chart.zoomTo(951350400000, 954201600000);
