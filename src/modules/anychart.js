43ba58ddb22dfe33b79948118b33ec01e53c0375
langley <clear.meltdown@gmail.com>
2014-12-16 12:18:47 +0800

    anygantt namespace fix

--- src/modules/anychart.js
+++ src/modules/anychart.js
@@ -1,7 +1,7 @@
 goog.provide('anychart.modules.anychart');
 
+goog.require('anychart.modules.anygantt');
 goog.require('anychart.modules.base');
 goog.require('anychart.modules.charts');
-goog.require('anychart.modules.gantt');
 
 
