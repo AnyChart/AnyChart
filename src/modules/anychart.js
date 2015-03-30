c8b4e3819492f946e9e4bb8192279508b5d1207f
roman_lubushkin <ra.lubushkin@gamil.com>
2015-03-30 08:19:15 +0800

    Move gauge to charts module

--- src/modules/anychart.js
+++ src/modules/anychart.js
@@ -3,7 +3,6 @@ goog.provide('anychart.modules.anychart');
 goog.require('anychart.modules.anygantt');
 goog.require('anychart.modules.base');
 goog.require('anychart.modules.charts');
-goog.require('anychart.modules.gauges');
 goog.require('anychart.modules.toolbar');
 
 
--- src/modules/charts.js
+++ src/modules/charts.js
@@ -3,6 +3,7 @@ goog.provide('anychart.modules.charts');
 goog.require('anychart.modules.base');
 goog.require('anychart.modules.bullet');
 goog.require('anychart.modules.cartesian');
+goog.require('anychart.modules.gauges');
 goog.require('anychart.modules.pie');
 goog.require('anychart.modules.polar');
 goog.require('anychart.modules.radar');
