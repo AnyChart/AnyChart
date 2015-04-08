113b61b94b79ba68d1c54f755874f527d6c3937c
chidori <kagakinam@gmail.com>
2015-04-08 12:35:54 +0800

    DVF-1325 fixed bug with tooltip padding

--- src/core/ui/TooltipItem.js
+++ src/core/ui/TooltipItem.js
@@ -633,9 +633,7 @@ anychart.core.ui.TooltipItem.prototype.draw = function() {
 
   if (this.hasInvalidationState(anychart.ConsistencyState.BOUNDS)) {
     this.calculateContentBounds_();
-    this.boundsWithoutPadding_ = this.padding_ ?
-        this.padding_.tightenBounds(/** @type {!anychart.math.Rect} */(this.contentBounds_)) :
-        this.contentBounds_;
+    this.boundsWithoutPadding_ = this.padding().tightenBounds(/** @type {!anychart.math.Rect} */(this.contentBounds_));
     this.titleRemainingBounds_ = null;
     this.separatorRemainingBounds_ = null;
     this.markConsistent(anychart.ConsistencyState.BOUNDS);
@@ -673,7 +671,7 @@ anychart.core.ui.TooltipItem.prototype.draw = function() {
     var separator = /** @type {anychart.core.ui.Separator} */(this.separator());
     separator.suspendSignalsDispatching();
     if (this.enabled() && this.visible()) separator.container(this.layer_);
-    separator.parentBounds(this.titleRemainingBounds_ || this.contentBounds_);
+    separator.parentBounds(this.titleRemainingBounds_ || this.boundsWithoutPadding_);
     separator.resumeSignalsDispatching(false);
     separator.draw();
 
@@ -686,7 +684,7 @@ anychart.core.ui.TooltipItem.prototype.draw = function() {
 
   if (this.hasInvalidationState(anychart.ConsistencyState.TOOLTIP_LABELS)) {
     var label = /** @type {anychart.core.ui.Label} */(this.content());
-    var remainingBounds = this.separatorRemainingBounds_ || this.titleRemainingBounds_ || this.contentBounds_;
+    var remainingBounds = this.separatorRemainingBounds_ || this.titleRemainingBounds_ || this.boundsWithoutPadding_;
     label.suspendSignalsDispatching();
     if (this.enabled() && this.visible()) label.container(this.layer_);
     label.parentBounds(remainingBounds);
@@ -717,9 +715,6 @@ anychart.core.ui.TooltipItem.prototype.calculateContentBounds_ = function() {
     var result = new anychart.math.Rect(0, 0, 0, 0);
     var separatorBounds;
 
-    var padding = /** @type {anychart.core.utils.Padding} */(this.padding());
-    padding.widenBounds(result);
-
     var title = /** @type {anychart.core.ui.Title} */(this.title());
     if (title.enabled()) {
       title.parentBounds(null);
@@ -744,7 +739,7 @@ anychart.core.ui.TooltipItem.prototype.calculateContentBounds_ = function() {
       result.height += separatorBounds.height;
     }
 
-    this.contentBounds_ = result;
+    this.contentBounds_ = this.padding().widenBounds(result);
   }
 };
 
