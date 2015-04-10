04103c42f5961d4f1eeb3cbc69f2edb5abc29dab
Sergey Medvedev <blackart.ru@gmail.com>
2015-04-10 16:12:10 +0800

    DVF-1398 VML grid problem

--- demos/grid/index.js
+++ demos/grid/index.js
@@ -1,48 +1,61 @@
-var chart, base;
+var chart;
+anychart.onDocumentReady(function() {
+  //create data set on our data
+  var dataSet = anychart.data.set([
+    ['Traffic', 56, 0],
+    ['Child Care', 44.8, 35],
+    ['Transp.', 27.2, 61],
+    ['Weather', 19.6, 77],
+    ['Overslept', 11.4, 89],
+    ['Emergency', 6.6, 100]
+  ]);
 
+  //map data for the first series, take x from the zero column and value from the first column of data set
+  var seriesData_1 = dataSet.mapAs({x: [0], value: [1]});
 
-function load() {
-  chart = new anychart.charts.Cartesian();
+  //map data for the second series, take x from the zero column and value from the second column of data set
+  var seriesData_2 = dataSet.mapAs({x: [0], value: [2]});
+
+  //create column chart
+  chart = anychart.column();
+
+  //set container id for the chart
   chart.container('container');
-  chart.bounds(10, 10, '100%', '100%');
-  chart.line([
-    [1, 10],
-    [2, 30],
-    [3 , 5],
-    [4, -6],
-    [6, 23],
-    [7, 15],
-    [8, 24],
-    [9, 19],
-    [10, 29],
-    [11, 2],
-    [12, -10],
-    [13, 5],
-    [14, 17],
-    [15, 21],
-    [16, 25],
-    [27, 37],
-    [18, 30],
-    [19, 27],
-    [20, 35],
-    [21, 40],
-  ]).markers(null);
 
-  chart.yAxis(0).orientation('left');
-  chart.xAxis(0).orientation('top');
-  chart.xAxis(1).orientation('bottom');
-  chart.yAxis(1).orientation('right');
+  //set chart title text settings
+  chart.title().text('Pareto Chart of Late Arrivals by Reported Cause');
 
-  chart.grid(0)
-      .direction('horizontal')
-      .invert(false)
-      .minor(true);
+  //create second series with mapped data
+  chart.column(seriesData_1);
 
-  chart.grid(1)
-      .direction('vertical')
-      .invert(false)
-      .oddFill('lightgray 0.1')
-      .evenFill('white 0.1');
+  //setup scale settings
+  var yScale = chart.yScale();
+  yScale.minimum(0);                    //set scale minimum value
+  yScale.maximum(184.8);                //set scale maximum value
+  yScale.ticks().interval(16.8);        //set scale ticks interval value
+  yScale.minorTicks().interval(8.4);   //set scale minor ticks interval value
 
+  //create additional scale for line series and extraYAxis
+  //it force line series to not stuck with over series
+  var additionalYScale = anychart.scales.linear();
+  additionalYScale.minimum(0);
+  additionalYScale.maximum(110);
+  additionalYScale.ticks().interval(10);
+
+  //create line series and set scale for it
+  var lineSeries = chart.line(seriesData_2);
+  lineSeries.yScale(additionalYScale);
+
+  //create extra axis on the right side of chart
+  var extraYAxis = chart.yAxis(1);
+  extraYAxis.orientation('right');
+  extraYAxis.scale(additionalYScale);
+
+  //setup axis to append '%' symbol to label values
+  extraYAxis.labels().textFormatter(function(info) {
+    return info.value + '%';
+  });
+
+  //initiate chart drawing
   chart.draw();
-}
+});
