d7e7d438936cd7345d3a9ce3ea5bf14b457eb0e6
chidori <kagakinam@gmail.com>
2016-01-21 15:27:33 +0800

    DVF-1945 fixed box outlier markers hover interactivity

--- src/core/Chart.js
+++ src/core/Chart.js
@@ -1264,6 +1264,8 @@ anychart.core.Chart.prototype.handleMouseOverAndMove = function(event) {
 
   if (series && !series.isDisposed() && series.enabled() && goog.isFunction(series.makePointEvent)) {
     var evt = series.makePointEvent(event);
+    if (index != evt['pointIndex'])
+      index = evt['pointIndex'];
     if (evt && ((anychart.utils.checkIfParent(/** @type {!goog.events.EventTarget} */(series), event['relatedTarget'])) || series.dispatchEvent(evt))) {
       if (interactivity.hoverMode() == anychart.enums.HoverMode.SINGLE) {
 
