dc009939d05cd945b18791624bad28ceeeb76525
Anton Saukh <grasshoppergn@gmail.com>
2016-09-22 13:41:31 +0800

    GraphicsJs added as a second contrib source for graphics. Switched building to it

--- demos/index.js
+++ demos/index.js
@@ -1,18 +1,22 @@
-anychart.onDocumentReady(function() {
-  var dataSet = anychart.data.set([
-    {id: "AU.CT", size: 40},
-    {id: "AU.VI", size: 70},
-    {id: "AU.WA", size: 90},
-    {id: "AU.QL", size: 50},
-    {id: "AU.NS", size: 80},
-    {id: "AU.NT", size: 60},
-    {id: "AU.TS", size: 40},
-    {id: "AU.SA", size: 50}
-  ]);
+var calendar, res;
 
-  var dataSetForSeries = dataSet.mapAs({id: "id"});
-  chart = anychart.bubbleMap(dataSetForSeries);
-  chart.geoData('anychart.maps.australia');
-  // chart.geoIdField("!");
-  chart.container('container').draw();
+anychart.onDocumentReady(function() {
+  calendar = anychart.scales.calendar();
+  calendar.availabilities([
+    {each: 'year', on: '2016/09/25'},
+    {on: '2016/09/25', from: '10:20', to: '14:15', isWorking: false},
+    {each: 'year', from: '2016/09/26', to: '2016/09/28'},
+    {each: 'w', on: 0, from: 10, to: 12},
+    {each: 'w', on: 0, from: 13, to: 16},
+    {each: 'w', on: 1, from: 10, to: 16},
+    {each: 'w', on: 1, from: 12, to: 13, isWorking: false},
+  ]);
+  res = calendar.getWorkingSchedule(new Date(2016, 8, 24, 0), new Date(2016, 8, 30, 0));
+  console.log(res.map(function(val) {
+    if (!val.length)
+      return "Holidays";
+    return anychart.format.dateTime(val[0][0], 'EEE dd MMM: \n') + val.map(function(val) {
+      return anychart.format.dateTime(val[0], 'HH:mm-') + anychart.format.dateTime(val[1], 'HH:mm');
+    }).join('\n');
+  }), res);
 });
