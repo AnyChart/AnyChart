fc5434825b24c55d59eaa9c2a5e5d574fa8ac462
chidori <kagakinam@gmail.com>
2016-12-08 16:01:06 +0800

    prepare project to opensource

---
+++ dist/themes/light_provence.js
@@ -0,0 +1,18 @@
+function a() {
+  return window.anychart.color.darken(this.sourceColor);
+}
+function b() {
+  return window.anychart.color.lighten(this.sourceColor);
+}
+window.anychart = window.anychart || {};
+window.anychart.themes = window.anychart.themes || {};
+window.anychart.themes.lightProvence = {palette:{type:"distinct", items:"#aa8ab3 #b7cbe2 #cdd18e #938d9c #6f5264 #96246a #519790 #6aabcc #61687d #7b8030".split(" ")}, ordinalColor:{autoColors:function(c) {
+  return window.anychart.color.blendedHueProgression("#e1bee7", "#9656a1", c);
+}}, defaultFontSettings:{fontFamily:'"Source Sans Pro", sans-serif', fontSize:13, fontColor:"#997f89"}, defaultBackground:{fill:"#ffffff", stroke:"#ffffff", cornerType:"round", corners:0}, defaultAxis:{stroke:"#dacfd3 0.8", title:{fontSize:15}, ticks:{stroke:"#dacfd3 0.8"}, minorTicks:{stroke:"#e5dcdf 0.8"}}, defaultGridSettings:{stroke:"#dacfd3 0.8"}, defaultMinorGridSettings:{stroke:"#e5dcdf 0.8"}, defaultSeparator:{fill:"#dacfd3 0.8"}, defaultTooltip:{title:{fontColor:"#745c65", padding:{bottom:5}, 
+fontSize:15}, content:{fontColor:"#997f89"}, separator:{enabled:!1}, fontColor:"#997f89", fontSize:13, background:{fill:"#ffffff 0.9", stroke:"1 #ebebeb", corners:0}, offsetX:15, offsetY:0, padding:{top:5, right:15, bottom:5, left:15}}, defaultColorRange:{stroke:"#c5c1c6", ticks:{stroke:"#c5c1c6", position:"outside", length:7, enabled:!0}, minorTicks:{stroke:"#c5c1c6", position:"outside", length:5, enabled:!0}, marker:{padding:{top:3, right:3, bottom:3, left:3}, fill:"#997f89", hoverFill:"#997f89"}}, 
+defaultScroller:{fill:"#f2ebf3", selectedFill:"#e2dce3", thumbs:{fill:"#d4ced5", stroke:"#997f89", hoverFill:"#c5c1c6", hoverStroke:"#997f89"}}, defaultLegend:{fontSize:13}, chart:{title:{fontSize:17}, padding:{top:20, right:25, bottom:15, left:15}}, cartesianBase:{defaultSeriesSettings:{candlestick:{risingFill:"#aa8ab3", risingStroke:"#aa8ab3", hoverRisingFill:b, hoverRisingStroke:a, fallingFill:"#b7cbe2", fallingStroke:"#b7cbe2", hoverFallingFill:b, hoverFallingStroke:a, selectRisingStroke:"3 #aa8ab3", 
+selectFallingStroke:"3 #b7cbe2", selectRisingFill:"#333333 0.85", selectFallingFill:"#333333 0.85"}, ohlc:{risingStroke:"#aa8ab3", hoverRisingStroke:a, fallingStroke:"#b7cbe2", hoverFallingStroke:a, selectRisingStroke:"3 #aa8ab3", selectFallingStroke:"3 #b7cbe2"}}}, pieFunnelPyramidBase:{labels:{fontColor:null}, connectorStroke:"#dacfd3", outsideLabels:{autoColor:"#997f89"}, insideLabels:{autoColor:"#212121"}}, map:{unboundRegions:{enabled:!0, fill:"#f2ebf3", stroke:"#e2dce3"}, linearColor:{colors:["#e1bee7", 
+"#aa8ab3", "#9656a1"]}, defaultSeriesSettings:{base:{stroke:"#b2aab5", labels:{fontColor:"#212121"}}, connector:{selectStroke:"1.5 #000", hoverStroke:"1.5 #b0bec5", stroke:"1.5 #aa8ab3", markers:{fill:"#b7cbe2", stroke:"1.5 #f2ebf3"}, hoverMarkers:{fill:"#b7cbe2"}}}}, sparkline:{padding:0, background:{stroke:"#ffffff"}, defaultSeriesSettings:{area:{stroke:"1.5 #aa8ab3", fill:"#aa8ab3 0.5"}, column:{fill:"#aa8ab3", negativeFill:"#b7cbe2"}, line:{stroke:"1.5 #aa8ab3"}, winLoss:{fill:"#aa8ab3", negativeFill:"#b7cbe2"}}}, 
+bullet:{background:{stroke:"#ffffff"}, defaultMarkerSettings:{fill:"#aa8ab3", stroke:"2 #aa8ab3"}, padding:{top:5, right:10, bottom:5, left:10}, margin:{top:0, right:0, bottom:0, left:0}, rangePalette:{items:["#848D90", "#98A0A3", "#B4B9BB", "#CFD3D4", "#EAECED"]}}, heatMap:{stroke:"1 #ffffff", hoverStroke:"1.5 #ffffff", selectStroke:"2 #ffffff", labels:{fontColor:"#212121"}}, treeMap:{headers:{background:{enabled:!0, fill:"#f2ebf3", stroke:"#e2dce3"}}, hoverHeaders:{fontColor:"#997f89", background:{fill:"#e2dce3"}}, 
+labels:{fontColor:"#212121"}, selectLabels:{fontColor:"#fafafa"}, stroke:"#e2dce3", selectStroke:"2 #eceff1"}};
+
---
+++ dist/themes/pastel.js
@@ -0,0 +1,17 @@
+function a() {
+  return window.anychart.color.darken(this.sourceColor);
+}
+function b() {
+  return window.anychart.color.lighten(this.sourceColor);
+}
+window.anychart = window.anychart || {};
+window.anychart.themes = window.anychart.themes || {};
+window.anychart.themes.pastel = {palette:{type:"distinct", items:"#90caf9 #80cbc4 #aed581 #e6ee9c #ffcc80 #ffab91 #f8bbd0 #d1c4e9 #9e9e9e #c7b299".split(" ")}, ordinalColor:{autoColors:function(c) {
+  return window.anychart.color.blendedHueProgression("#e6ee9c", "#80cbc4", c);
+}}, defaultFontSettings:{fontColor:"#9b8b7e"}, defaultBackground:{fill:"#fefdfa", stroke:"#f1eeea", cornerType:"round", corners:0}, defaultTooltip:{fontSize:12, background:{fill:"#eceff1 0.94", stroke:"1.5 #e3e3e3"}, padding:{top:8, right:15, bottom:10, left:15}}, defaultColorRange:{stroke:"#bdbdbd", ticks:{stroke:"#bdbdbd", position:"outside", length:7, enabled:!0}, minorTicks:{stroke:"#bdbdbd", position:"outside", length:5, enabled:!0}, marker:{padding:{top:3, right:3, bottom:3, left:3}, fill:"#37474f", 
+hoverFill:"#37474f"}}, defaultScroller:{fill:"#f2efec", selectedFill:"#dcd8d4", thumbs:{fill:"#F9FAFB", stroke:"#bdc8ce", hoverFill:"#bdc8ce", hoverStroke:"#e9e4e4"}}, defaultAxis:{stroke:"#9b8b7e 0.4", ticks:{stroke:"#9b8b7e 0.4"}, minorTicks:{stroke:"#9b8b7e 0.2"}}, chart:{padding:{top:20, right:25, bottom:15, left:15}}, cartesianBase:{defaultSeriesSettings:{candlestick:{risingFill:"#90caf9", risingStroke:"#90caf9", hoverRisingFill:b, hoverRisingStroke:a, fallingFill:"#ffcc80", fallingStroke:"#ffcc80", 
+hoverFallingFill:b, hoverFallingStroke:a, selectRisingStroke:"3 #90caf9", selectFallingStroke:"3 #ffcc80", selectRisingFill:"#333333 0.85", selectFallingFill:"#333333 0.85"}, ohlc:{risingStroke:"#90caf9", hoverRisingStroke:a, fallingStroke:"#ffcc80", hoverFallingStroke:a, selectRisingStroke:"3 #90caf9", selectFallingStroke:"3 #ffcc80"}}}, defaultGridSettings:{stroke:"#9b8b7e 0.4"}, defaultMinorGridSettings:{stroke:"#9b8b7e 0.2"}, defaultSeparator:{fill:"#9b8b7e 0.4"}, map:{unboundRegions:{enabled:!0, 
+fill:"#f2efec", stroke:"#dcd8d4"}, linearColor:{colors:["#e6ee9c", "#80cbc4"]}, defaultSeriesSettings:{base:{stroke:"#eceff1", labels:{fontColor:"#212121"}}, connector:{stroke:"1.5 #90caf9", hoverStroke:"1.5 #37474f", selectStroke:"1.5 #000", markers:{stroke:"1.5 #e9e6e3", fill:"#80cbc4"}, hoverMarkers:{fill:"#80cbc4"}}}}, sparkline:{padding:0, background:{stroke:"#fefdfa"}, defaultSeriesSettings:{area:{stroke:"1.5 #90caf9", fill:"#90caf9 0.5"}, column:{fill:"#90caf9", negativeFill:"#ffcc80"}, line:{stroke:"1.5 #90caf9"}, 
+winLoss:{fill:"#90caf9", negativeFill:"#ffcc80"}}}, bullet:{background:{stroke:"#fefdfa"}, defaultMarkerSettings:{fill:"#90caf9", stroke:"2 #90caf9"}, padding:{top:5, right:10, bottom:5, left:10}, margin:{top:0, right:0, bottom:0, left:0}, rangePalette:{items:["#CDCDCC", "#D7D7D5", "#E4E3E2", "#EEEDEB", "#F8F7F4"]}}, heatMap:{stroke:"1 #fefdfa", hoverStroke:"1.5 #fefdfa", selectStroke:"2 #fefdfa", labels:{fontColor:"#212121"}}, treeMap:{headers:{background:{enabled:!0, fill:"#f2efec", stroke:"#dcd8d4"}}, 
+hoverHeaders:{fontColor:"#757575", background:{fill:"#dcd8d4"}}, labels:{fontColor:"#212121"}, selectLabels:{fontColor:"#9b8b7e"}, stroke:"#dcd8d4", selectStroke:"2 #eceff1"}};
+
