7816a7ab8bae8e1db8ca8f8c608d0b7883ae249e
chidori <kagakinam@gmail.com>
2016-12-08 13:01:32 +0800

    prepare project to opensource

--- demos/index.js
+++
@@ -1,9 +0,0 @@
-var chart;
-anychart.onDocumentReady(function() {
-  anychart.licenseKey('test-key-32db1f79-cc9312c4');
-  chart = anychart.area([10, 14, 13, 12, 10], [12, 10, 14, 13, 12], [15, 12, 10, 14, 13]);
-  //chart.credits(false);
-  chart.animation(10000);
-  chart.container('container');
-  chart.draw();
-});
