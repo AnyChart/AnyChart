18c7133e488e7afd86becd95a7b27f252ee1732d
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-06-28 12:17:45 +0800

    Disable all stock mouse wheel scrolls by default

--- src/themes/defaultTheme.js
+++ src/themes/defaultTheme.js
@@ -4902,7 +4902,7 @@ goog.provide('anychart.themes.defaultTheme');
       'zoomMarqueeStroke': '#d3d3d3',
       'interactivity': {
         'zoomOnMouseWheel': false,
-        'scrollOnMouseWheel': true
+        'scrollOnMouseWheel': false
       }
     },
 
