e5b70bd66f4e6017720e6effa094830296f14701
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-11-10 11:12:37 +0800

    Update version to 8.0.2

--- src/utils.js
+++ src/utils.js
@@ -1250,7 +1250,7 @@ anychart.utils.json2xml = function(json, opt_rootNodeName, opt_returnAsXmlNode) 
   var root = anychart.utils.json2xml_(json, opt_rootNodeName || 'anychart', result);
   if (root) {
     if (!opt_rootNodeName)
-      root.setAttribute('xmlns', 'http://anychart.com/schemas/8.0.1/xml-schema.xsd');
+      root.setAttribute('xmlns', 'http://anychart.com/schemas/8.0.2/xml-schema.xsd');
     result.appendChild(root);
   }
   return opt_returnAsXmlNode ? result : goog.dom.xml.serialize(result);
