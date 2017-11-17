f48587efacac987ebe000980e5411a92f56926c6
Roman Lubushkin <ra.lubushkin@gmail.com>
2017-11-17 11:39:09 +0800

    Fixed graphics output path

--- bin/build_release.sh
+++ bin/build_release.sh
@@ -13,5 +13,5 @@ cd libs/graphicsjs
 python build.py plain
 python build.py compile
 cd ../../
-cp libs/graphicsjs/out/graphics.js dist/graphics.js
-cp libs/graphicsjs/out/graphics.min.js dist/graphics.min.js
+cp libs/graphicsjs/out/graphics.js dist/js/graphics.js
+cp libs/graphicsjs/out/graphics.min.js dist/js/graphics.min.js
